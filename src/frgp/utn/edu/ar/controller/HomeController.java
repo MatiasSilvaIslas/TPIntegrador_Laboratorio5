c5d0361f5fb3897689a65a275278853822e1c12d
nicosj <nico.river09.9@gmail.com>
2024-06-21 20:14:47 -0300

    listo

--- src/frgp/utn/edu/ar/controller/HomeController.java
+++ src/frgp/utn/edu/ar/controller/HomeController.java
@@ -1,8 +1,16 @@
 package frgp.utn.edu.ar.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
 
+    @RequestMapping("/")
+    public ModelAndView home() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("index");
+        return modelAndView;
+    }
 }
